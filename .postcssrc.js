0877b258bf0a5e3fa9a34a8b046ee554364924ab
jiangchengwei <13521766253@163.com>
2020-09-01 23:44:00 +0800

    代码配置添加注释

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,8 +1,9 @@
 /*
  * @description:
  * @Date: 2020-08-25 18:42:58
- * @LastEditTime: 2020-08-30 23:47:48
+ * @LastEditTime: 2020-09-01 21:08:04
  */
 module.exports = {
+  // 配置移动端项目
   // plugins: { 'postcss-pxtorem': { rootValue: 32, propList: ['*'], minPixelValue: 2 }}
 }
--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,13 @@
 /*
  * @description: 
  * @Date: 2020-08-25 18:27:35
- * @LastEditTime: 2020-08-31 16:16:04
+ * @LastEditTime: 2020-09-01 21:07:54
  */
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
+  // 配置模块的按需加载
   // "plugins": [
   //   [
   //     "component",
